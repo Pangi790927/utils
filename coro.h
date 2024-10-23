341cd87a1b3abb5d407cb20fa0d7f615142ba39f
Andrei Pangratie <apangratie@technovolt.ro>
2024-10-23 19:05:49 +0300

    rethinking coro

---
+++ coro.h
@@ -0,0 +1,416 @@
+#ifndef CORO_H
+#define CORO_H
+/* rewrite of co_utils.h ->
+    - CPP library over the CPP corutines
+    - Built around epoll (ie, epoll is the only blocking function)
+    - Meant to have one pool/thread or enable mutexes on pool (this will allow callbacks and pool
+    sharing)
+    - Meant to have modifs (callbacks on corutine execution)
+*/
+
+/* The maximum amount of concurent timers that can be awaited */
+#ifndef CO_MAX_TIMER_POOL_SIZE
+# define CO_MAX_TIMER_POOL_SIZE 64
+#endif
+
+/* Set if a pool can be available in more than one thread, necesary if you want to transfer the pool
+to a callback that will be called from another thread */
+#ifndef CO_ENABLE_MULTITHREADS
+# define CO_ENABLE_MULTITHREADS false
+#endif
+
+/* If set to true, will enable the debug callbacks */
+#ifndef CO_ENABLE_CALLBACKS
+# define CO_ENABLE_CALLBACKS false
+#endif
+
+/* If set to true, will enable the logging callback */
+#ifndef CO_ENABLE_LOGGING
+# define CO_ENABLE_LOGGING true
+#endif
+
+/* If set to true, makes names for corutines */
+#ifndef CO_ENABLE_DEBUG_NAMES
+# define CO_ENABLE_DEBUG_NAMES false
+#endif
+
+/* If set, will create a debug tracer and it will add it to all coroutines */
+#ifndef CO_ENABLE_DEBUG_TRACE_ALL
+# define CO_ENABLE_DEBUG_TRACE_ALL false
+#endif
+
+#if !defined(CO_KILL_DEFAULT_CONSTRUCT) && !defined(CO_KILL_DEFAULT_CONSTRUCT)
+# define CO_KILL_DEFAULT_CONSTRUCT true
+# define CO_KILL_RAISE_EXCEPTION false
+#endif
+
+#if defined(CO_KILL_DEFAULT_CONSTRUCT) && defined(CO_KILL_DEFAULT_CONSTRUCT)
+# error "Don't choose both"
+#endif
+
+/* If CO_ENABLE_DEBUG_NAMES you can also define CORO_REG and use it to register a corutine's name */
+#if CO_ENABLE_DEBUG_NAMES
+# ifndef CORO_REG
+#  define CORO_REG(t)   coro::dbg_register_name(t, "%20s:%5d:%s", __FILE__, __LINE__, #t)
+# endif
+#else
+# define CORO_REG(t)    t
+#endif
+
+namespace coro {
+
+constexpr int MAX_TIMER_POOL_SIZE = CO_MAX_TIMER_POOL_SIZE;
+
+/* corutine return type, this is the result of creating a corutine, you can await it and also pass
+it around (practically std::coroutine_handle, but can cast itself into things) */
+template<typename RetType> struct task;
+
+/* A pool is the shared state between all corutines. This object holds the epoll handler, timers,
+queues, etc. Each corutine has a pointer to this object. You can see this object as an instance of
+epoll. */
+struct pool_t;
+
+/* Modifs, corutine modifications. Those modifications controll the way a corutine behaves when
+awaited or when spawning a new corutine from it. Those modifications can be inherited, making all
+the corutines in the call sub-tree behave in a similar way. For example: adding a timeouts, for this
+example, all the corutines that are on the same call-path(not spawn-path) would have a timer, such
+that the original call would not exist for more than X time units. (+/- the code between awaits) */
+struct modif_t;
+
+/* TODO: think this better this time, including better lifetime */
+/* This is a semaphore working on a pool. It can be awaited to decrement it's count and .rel()
+increments it's count from wherever. More bellow. */
+struct sem_t;
+
+/* used pointers, so _p marks a shared_pointer and a _wp marks a weak pointer. If the pool holds
+the object you will get a weak pointer and if you are to hold the object you will get a shared
+pointer. */
+using pool_p = std::shared_ptr<pool_t>;
+using pool_wp = std::weak_ptr<pool_t>;
+using modif_p = std::shared_ptr<modif_t>;
+using modif_wp = std::weak_ptr<modif_t>;
+
+enum error_e : int32_t {
+    ERROR_OK      = 0,
+    ERROR_GENERIC = -1, /* the source is not specified, can use log_str to find the error,
+                        or sometimes errno */
+    ERROR_TIMEOUT = -2, /* the error comes from a modif, namely a timeout */
+    ERROR_WAKEUP  = -3, /* the error comes from force awaking the awaiter */
+    ERROR_USER    = -4, /* the error comes from a modif, namely an user defined modif, users can
+                        use this if they wish to return from modif cbks */
+    ERROR_DEPEND  = -5, /* the error comes from a depend modif, i.e. depended function failed */
+};
+
+enum run_e : int32_t {
+    RUN_OK,
+    RUN_DONE,
+    RUN_ERRORED,
+    RUN_STOPPED, /* can be re-run (comes from force_stop) */
+};
+
+/* all the internal tasks return this */
+using task_t = task<error_e>;
+
+/* some internal structures that are not part of the api, but are needed for the
+functions/structures bellow
+*/
+struct yield_awaiter_t;
+struct sched_awaiter_t;
+struct pool_internal_t;
+struct sem_internal_t;
+
+#if CO_EXCEPT_ON_KILL
+/* TODO: the exception that will be thrown by the killing of non default constructible types */
+#endif
+
+struct pool_t {
+    void sched(task_t task, const std::vector<modif_p>& v = {}); /* ignores return type */
+    run_e run();
+
+    std::unique_ptr<pool_internal_t> internal;
+};
+
+struct sem_t {
+    struct unlocker_t{ /* compatibility with guard objects ex: std::lock_guard guard(co_await s); */
+        sem_t &sem;
+        unlocker_t(sem_t &sem) : sem(sem) {}
+        void lock() {}
+        void unlock() { sem.inc(); }
+    };
+
+    /* can be created and moved, but not copied */
+    sem_t(pool_wp pool);
+    sem_t(sem_t& sem) = delete;
+    sem_t(sem_t&& sem) { internal = std::move(sem.internal); }
+    sem_t &operator = (sem_t& sem) = delete;
+    sem_t &operator = (sem_t&& sem) { internal = std::move(sem.internal); return *this; }
+
+    void inc();
+    bool try_dec();
+    /* await instead of dec() */
+
+    template <typename ret_t>
+    task<ret_t> await_suspend(task<ret_t> to_suspend);
+    bool        await_ready();
+    unlocker_t  await_resume();
+private:
+    std::unique_ptr<sem_internal_t> internal;
+};
+
+struct modif_t {
+    /* called when a new corutine is called (co_await on it) */
+    std::fucntion<error_e(task_t, modif_t *)> call_cbk;
+
+    /* called when a new corutine that is newly scheduled (co::sched on it) */
+    std::fucntion<error_e(task_t, modif_t *)> sched_cbk;
+
+    /* called when the return is valid (co_return) */
+    std::fucntion<error_e(task_t, modif_t *)> exit_cbk;
+
+    /* called on a coroutine before it starts/stops execution (co_await, ?co_yield? something else)*/
+    std::fucntion<error_e(task_t, modif_t *)> leave_cbk;
+    std::fucntion<error_e(task_t, modif_t *)> enter_cbk;
+
+    /* awaiting a file descriptor */
+    std::fucntion<error_e(task_t, int fd, int wait_cond, modif_t *)> waitfd_cbk;
+    std::fucntion<error_e(task_t, int fd, int res, int err_no, modif_t *)> unwaitfd_cbk;
+
+    /* awaiting on a semaphore */
+    std::function<error_e(task_t, sem_t *, modif_t *)> waitsem_cbk;
+    std::function<error_e(task_t, sem_t *, modif_t *)> unwaitsem_cbk;
+
+    /* the following callbacks specify how this modifier is inherited on diverse calls, if the
+    function does not exist, the mod is not inherited, if it exists, it is called with itself and
+    the result is used further on. You can at those points decide to create a new modifier for the
+    call or keep the old one.
+    When called, all the caller's modifs are inherited and it's modifs(if any) are kept.  */
+    std::fucntion<modif_p(modif_p)> inherit_call_cbk;
+    std::fucntion<modif_p(modif_p)> inherit_sched_cbk;
+
+    /* those are called by the contructor/deconstructor. */
+    std::fucntion<void(modif_t *)> init_cbk;
+    std::fucntion<void(modif_t *)> uninit_cbk;
+
+    /* If a modif is added twice(same address) then this callback is called. */
+    std::fucntion<void(modif_t *)> same_cbk;
+
+    modif_t() { init_cbk(this); };
+    ~modif_t() { uninit_cbk(this); };
+
+    /* do whatever you want with this */
+    void *ctx = NULL;
+
+    /* don't ever touch this */
+    void *internal = NULL;
+};
+
+/* Pool & Sched functions:
+------------------------------------------------------------------------------------------------  */
+
+inline pool_p create_pool();
+
+/* gets the pool of the current corutine, necesary if you want to sched corutines from callbacks.
+Don't forget you need multithreading enabled if you want to schedule from another thread. */
+inline task<pool_wp> get_pool();
+
+/* This does not stop the curent corutine, it only schedules the task, but does not yet run it */
+template <typename ret_t>
+inline sched_awaiter_t sched(task<ret_t> to_sched, const std::vector<modif_p>& v = {});
+
+/* This stops the current coroutine from running and places it at the end of the ready queue. */
+inline yield_awaiter_t yield();
+
+/* Modifications
+------------------------------------------------------------------------------------------------  */
+inline modif_p create_modif(const modif_t& modif_template);
+
+/* This returns the internal set that holds the different modifications of the task. */
+template <typename ret_t>
+inline std::set<client_wp, std::owner_less<client_wp>>> &task_modifs(task<ret_t> t);
+
+/* This adds a modifier to the task and returns the task */
+template <typename ret_t>
+inline task<ret_t> add_modif(task<ret_t> t, modif_p mod);
+
+/* Timing
+------------------------------------------------------------------------------------------------  */
+
+/* creates a timeout modification, i.e. the corutine will be stopped from executing if the given
+time passes */
+inline modif_p creat_timeo(const std::chrono::microseconds& timeo);
+
+/* sleep functions */
+inline task_t sleep_us(uint64_t timeo_us);
+inline task_t sleep_ms(uint64_t timeo_ms);
+inline task_t sleep_s(uint64_t timeo_s);
+inline task_t sleep(const std::chrono::milliseconds& us);
+
+/* Flow Controll:
+------------------------------------------------------------------------------------------------  */
+
+/* Observation: killed corutines, timed, result of functions that depend on others, futures, all
+of those will not be able to construct their return type if they are killed so those
+need to be handled, so you must choose that by defining one of the two: CO_KILL_DEFAULT_CONSTRUCT,
+CO_KILL_RAISE_EXCEPTION ?TODO:CHECK?
+
+Those that return error_e will return their apropiate error in this case, the rest will either
+have the error returned in the exception or set in the task/awaiter object.
+
+If CO_KILL_DEFAULT_CONSTRUCT, then the return value will be the default constructed type.(except if
+type is error_e).
+*/
+
+/* Creates semaphores, those need the pool to exist */
+inline sem_t create_sem(pool_wp pool);
+inline task<sem_t> create_sem();
+
+/* a modification that depends on the pool, this is used in scheduled coroutines to end the current
+corutine if they die. This is not inherited by either calls or spawns, but it is inherited by other
+calls to creat_depend further down. */
+inline task<modif_p> creat_depend();
+
+/* modifier that, once signaled, will awake the corutine and make it return ERROR_WAKEUP. If the
+corutine is not waiting it will return as soon as it reaches any co_await ?or co_yield? */
+/* TODO: what to do if not basic type? except? */
+/* TODO: don't forget to awake the fds nicely(something like stopfd) */
+inline modif_p creat_killer();
+inline void sig_killer(modif_p p);
+
+/* takes a task and adds the requred modifications to it such that the returned object will be
+returned once the return value of the task is available so:
+    auto t = co_task();
+    auto fut = co::create_future(t)
+    co_await co::sched(t);
+    ...
+    co_await fut; // returns the value of co_task once it has finished executing 
+*/
+template <typename ret_t>
+inline task<ret_t> creat_future(task<ret_t> t);
+
+/* wait for all the tasks to finish, the return value can be found in the respective task, killing
+one kills all (sig_killer installed in all). The inheritance is the same as with 'call'. */
+template <typename ...ret_v>
+inline task_t wait_all(task_t<ret_v>... tasks);
+
+/* calls an awaitable inside a task_t, this is done to be able to use modifs on awaitables */
+template <typename Awaiter>
+inline task_t await(Awaiter& awaiter);
+
+/* causes the running pool::run to stop, the function will stop at this point, can be resumed with
+another run */
+inline task_t force_stop(int ret);
+
+/* EPOLL & Linux Specific:
+------------------------------------------------------------------------------------------------  */
+
+/* convenience functions provided: */
+inline task_t connect(int fd, sockaddr *sa, socklen_t *len);
+inline task_t accept(int fd, sockaddr *sa, socklen_t *len);
+inline task_t read(int fd, void *buff, size_t len);
+inline task_t write(int fd, const void *buff, size_t len);
+inline task_t read_sz(int fd, void *buff, size_t len);
+inline task_t write_sz(int fd, const void *buff, size_t len);
+
+/* event can be EPOLLIN or EPOLLOUT (maybe some others work too?) This can be used to be notified
+when those are ready without doing a read or a write. */
+inline task_t wait_event(int fd, int event);
+
+/* closing a file descriptor while it is managed by the coroutine pool will break the entire system
+so you must use stopfd on the file descriptor before you close it. This makes sure the fd is
+awakened and ejected from the system before closing it. For example:
+
+    co_await co::stopfd(fd);
+    close(fd);
+*/
+inline task_t stopfd(int fd);
+
+/* Debug Interfaces:
+------------------------------------------------------------------------------------------------  */
+
+/* Registers a name for a given task */
+template <typename ret_t, typename ...Args>
+inline task<ret_t> dbg_register_name(task<ret_t> t, const char *fmt, task<ret_t>&&...);
+
+/* creates a modifier that traces things from corutines, mostly a convenience function, it also
+uses the log_str function, or does nothing else if it isn't defined. If you don't like the
+verbosity, be free to null any callback you don't care about. */
+inline modif_p creat_dbg_tracer();
+
+/* Obtains the name given to the respective task */
+template <typename ret_t>
+inline std::string dbg_name(task<ret_t> t);
+
+/* corutine callbacks for diverse points in the code, if in a multithreaded environment, you must
+take all the locks before rewriting those. The pool locks are held when those callbacks are called
+*/
+#if CO_ENABLE_CALLBACKS
+/* add callbacks here */
+inline std::function<void(void)> on_call;
+#endif /* CO_ENABLE_CALLBACKS */
+
+/* calls log_str to save the log string */
+#if CO_ENABLE_LOGGING
+inline std::function<int(const std::string&)> log_str;
+#endif
+
+/* IMPLEMENTATION 
+=================================================================================================
+=================================================================================================
+================================================================================================= */
+
+/* holds fd and things... */
+struct pool_internal_t {
+
+};
+
+/* This is the object with which you wait for a file descriptor, you create it with a valid
+wait_cond and fd and co_await on it. The resulting integer must be positive to not be an error */
+template <typename ret_t>
+struct fd_awaiter_t {
+    int wait_cond;  /* set with epoll wait condition */
+    int fd;         /* set with waited file descriptor */
+
+    std::coroutine_handle<void> await_suspend(task<ret_t> t);
+    bool                        await_ready();  /* never */
+    error_e                     await_resume(); /* resume  */
+private:
+    task<ret_t> caller; /* who to wake up */
+    pool_wp _pool;      /* the pool that holds this coroutine */
+};
+
+/* Those are the types of modifications that a corutine can have, including user created ones. */
+/* TODO: This should also dissapear once the object has callbacks, simply implement our own modif
+for timer and trace */
+// enum modif_type_e : int32_t {
+//     MODIF_TIMEOUT,
+//     MODIF_TRACE,
+
+//     MODIF_FIRST_USABLE,
+//     /* MODIF_FIRST_USABLE marks the first number that can be used by the user, as to not interact
+//     with the system. To be explicit, this means the user can add it's own modifications as long
+//     as those are defined as numbers larger than MODIF_FIRST_USABLE. */
+// };
+
+/* Those are the points that the pmods should be called */
+/* TODO: may not be needed if the function callbacks are not set, we don't call them, will remove
+them after writing modif_t */
+// enum modif_type_call_e : int32_t {
+//     MODIF_CALL_CALL         = 0x01, /* called when a new corutine is called (created and awaited) */    
+//     MODIF_CALL_SPAWN        = 0x02, /* called when a new corutine is scheduled (create and sched) */
+
+//     MODIF_CALL_LEAVE        = 0x04, /* called when the corutine is leaving the execution */
+//     MODIF_CALL_REENTRY      = 0x08, /* called when the corutine is resuming execution */
+//     MODIF_CALL_EXIT         = 0x10, /* called when the corutine is exiting */
+
+//     MODIF_CALL_FD_WAIT      = 0x20, /* called when the corutine awaits a file descriptor */
+//     MODIF_CALL_FD_UNWAIT    = 0x40, /* called when the corutine stops waiting on a file descriptor */
+//     MODIF_CALL_SEM_WAIT     = 0x80, /* called when the corutine starts waiting on a semaphore */
+//     MODIF_CALL_SEM_UNWAIT   = 0x100,/* called when the corutine stops waiting on a semaphore */
+// };
+
+}
+
+// comment this if you don't like it
+using co = coro;
+
+#endif
