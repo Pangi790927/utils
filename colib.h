df1c87334c2ce3413093e675c8c2185cfc8bc5f2
Andrei Pangratie <andrpangr@gmail.com>
2025-05-21 09:43:08 +0300

    version string added

--- colib.h
+++ colib.h
@@ -668,6 +668,15 @@ AcceptEx(...) ~> BOOL
 # include <windows.h>
 #endif
 
+/*! The version is formated as MAJOR,MINOR,DETAIL, where:
+    @MAJOR  - breaking changes to the interface: functions are deleted, functionality is added or
+              removed to already existing functions, etc.
+    @MINOR  - changes to the interface: functions are added, functionality is added to new
+              functons, additional default parameters are added, etc.
+    @DETAIL - fixes, implementation fixes, comments, etc. Changes that won't change the way you
+              use this library */
+#define COLIB_VERSION 0,0,1
+
 #if COLIB_OS_UNKNOWN
 /* you should include your needed files before including this file */
 #endif
