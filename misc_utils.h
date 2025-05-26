d721e84ecd8511b173eb1fc4dbcc391330c2e559
Andrei Pangratie <andrpangr@gmail.com>
2025-05-26 11:52:52 +0300

    has_ending

--- misc_utils.h
+++ misc_utils.h
@@ -259,4 +259,11 @@ inline std::vector<std::string> ssplit_empty(const std::string& src, const std::
     return ret;
 }
 
+inline bool has_ending(const std::string& full_string, const std::string& ending) {
+    if (full_string.length() >= ending.length())
+        return (0 == full_string.compare(
+                full_string.length() - ending.length(), ending.length(), ending));
+    return false;
+}
+
 #endif
