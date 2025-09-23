ef57b762c0291ee9879bd8234b17990df242fd3a
Andrei Pangratie <apangratie@technovolt.ro>
2025-09-23 18:38:50 +0300

    commetns

--- generic/voronoi.h
+++ generic/voronoi.h
@@ -1,6 +1,13 @@
 #ifndef VORONOI_H
 #define VORONOI_H
 
-
+/*! @file
+ * 
+ * Voronoi Diagrams - Delaunay Triangulation
+ * 
+ * Inspiration:
+ * https://www.vldb.org/pvldb/vol15/p246-lu.pdf
+ * 
+ * */
 
 #endif
